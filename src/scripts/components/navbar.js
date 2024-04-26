ce4032f8956dd071af2b03c81f2ba6d919aaec97
Dmitri49 <dmitrilissin@gmail.com>
2024-04-26 18:49:39 +0300

    add header

---
+++ src/scripts/components/navbar.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector("#navbar");
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+    if (scrollY > lastScrollTop) {
+      navbar.classList.remove("visible");
+    } else if (scrollY < lastScrollTop) {
+      navbar.classList.add("visible");
+    }
+    lastScrollTop = scrollY <= 0 ? 0 : scrollY;
+  },
+);
+
+
+// ================= CHECKBOX ====================== //
+
+const checkbox = document.querySelector('#checkbox');
+const wrapper = document.querySelector('#wrapper');
+
+checkbox.addEventListener('change', function(e) {
+    if (e.target.checked) {
+        wrapper.classList.add('light')
+    } else {
+        wrapper.classList.remove('light')
+    }
+
+})
--- src/scripts/index.js
+++
@@ -1,2 +0,0 @@
-const a = 500
-const b = 600
