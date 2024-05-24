80017b05c70f8e164fdcf36bc9c19eb363fa47e9
Dmitri49 <dmitrilissin@gmail.com>
2024-05-24 11:39:25 +0300

    add translate

--- src/scripts/components/navbar.js
+++ src/scripts/components/navbar.js
@@ -34,27 +34,25 @@ window.addEventListener("scroll", () => {
 
 // ================= LIGHT THEMA ====================== //
 
-const checkbox = document.querySelector('#checkbox');
-const wrapper = document.querySelector('#wrapper');
-const aboutSection = document.querySelector('#about_section');
-const skillsSection = document.querySelector('#skills_section');
-const worksSection = document.querySelector('#works_section');
-const contactSection = document.querySelector('#contact_section')
+// const checkbox = document.querySelector('#checkbox');
+// const wrapper = document.querySelector('#wrapper');
+// const aboutSection = document.querySelector('#about_section');
+// const skillsSection = document.querySelector('#skills_section');
+// const worksSection = document.querySelector('#works_section');
+// const contactSection = document.querySelector('#contact_section')
 
-checkbox.addEventListener('change', function(e) {
-    if (e.target.checked) {
-        wrapper.classList.add('light');
-        aboutSection.classList.add('light');
-        skillsSection.classList.add('light');
-        worksSection.classList.add('light');
-        contactSection.classList.add('light');
-    } else {
-        wrapper.classList.remove('light');
-        aboutSection.classList.remove('light');
-        skillsSection.classList.remove('light');
-        worksSection.classList.remove('light');
-        contactSection.classList.remove('light');
-    }
-
-})
-
+// checkbox.addEventListener('change', function(e) {
+//     if (e.target.checked) {
+//         wrapper.classList.add('light');
+//         aboutSection.classList.add('light');
+//         skillsSection.classList.add('light');
+//         worksSection.classList.add('light');
+//         contactSection.classList.add('light');
+//     } else {
+//         wrapper.classList.remove('light');
+//         aboutSection.classList.remove('light');
+//         skillsSection.classList.remove('light');
+//         worksSection.classList.remove('light');
+//         contactSection.classList.remove('light');
+//     }
+// })
